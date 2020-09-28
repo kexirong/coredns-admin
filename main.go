4834fe8240cce54be90751fa00b050ab2a538545
kexirong <kexr123!@#>
2020-09-28 15:17:11 +0800

    complete v0.1

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/kexirong/coredns-admin/config"
 	"github.com/kexirong/coredns-admin/router"
@@ -15,8 +17,30 @@ func main() {
 		panic(err)
 	}
 
-	err = router.Router.Run(fmt.Sprintf("%s:%s", conf.Host, conf.Port))
-	panic(err)
-	// client
-	// curl http://127.0.0.1:8080/JSONP?callback=x
+	var cmd = "run"
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+	switch cmd {
+	case "run":
+		err = router.Router.Run(fmt.Sprintf("%s:%s", conf.Host, conf.Port))
+		panic(err)
+	case "createuser":
+		if len(os.Args) != 4 {
+			fmt.Println("usage: createuser username password")
+		} else {
+			path := conf.UserEtcdPath
+			if !strings.HasSuffix(path, "/") {
+				path += "/"
+			}
+			path += os.Args[2]
+			secret := service.MakeSecret(os.Args[3])
+			err := service.EtcdPutKv(path, secret)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("user %s created\n", os.Args[2])
+		}
+	}
+
 }
